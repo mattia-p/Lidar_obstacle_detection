c1efcdf6e3f5c771b553b5bc75cbdeba9f058c32
Mattia <mattia_pelissou@berkeley.edu>
2020-04-29 23:45:18 -0700

    Create RANSAC for plane segmentation

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -119,17 +119,89 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 
 }
 
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+
+	// TODO: Fill in this function
+
+	// For max iterations
+    while(maxIterations--)
+    {
+        std::unordered_set<int> inliers;
+        while (inliers.size() < 3)
+            inliers.insert(rand()%(cloud->points.size()));
+
+        float x1, y1, z1, x2, y2, z2, x3, y3, z3;
+        auto itr = inliers.begin();
+        x1 = cloud->points[*itr].x;
+        y1 = cloud->points[*itr].y;
+        z1 = cloud->points[*itr].z;
+        itr++;
+        x2 = cloud->points[*itr].x;
+        y2 = cloud->points[*itr].y;
+        z2 = cloud->points[*itr].z;
+        itr++;
+        x3 = cloud->points[*itr].x;
+        y3 = cloud->points[*itr].y;
+        z3 = cloud->points[*itr].z;
+
+        float a = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+        float b = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+        float c = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+        float d = -(a * x1 + b * y1 + c * z1);
+
+
+        for(int index = 0; index < cloud->points.size(); index++)
+        {
+            if(inliers.count(index) >0)
+                continue;
+            pcl::PointXYZ point = cloud->points[index];
+            float x4 = point.x;
+            float y4 = point.y;
+            float z4 = point.z;
+
+            float d = fabs(a*x4 + b*y4 + c*z4 + d)/sqrt(a*a + b*b + c*c);
+
+            if(d <= distanceTol)
+                inliers.insert(index);
+        }
+
+        if(inliers.size() > inliersResult.size())
+        {
+            inliersResult = inliers;
+        }
+    }
+
+	// Randomly sample subset and fit line
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+
+	return inliersResult;
+
+}
+
 int main ()
 {
 
 	// Create viewer
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
-	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	// Create data for line example
+	// pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
 
-	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 10, 1.0);
+    // Case for plane example
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
+
+	// Case fpr line example
+	// std::unordered_set<int> inliers = Ransac(cloud, 10, 1.0);
+
+    // Case for plane example
+    std::unordered_set<int> inliers = RansacPlane(cloud, 30, 0.2);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
